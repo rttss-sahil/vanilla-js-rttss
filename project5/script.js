bdddfab568d5dec5e082650e8e83a5614dd3fcc4
rttss <sahilrathee55@gmail.com>
2020-07-10 21:08:18 +0530

    d

--- project5/script.js
+++ project5/script.js
@@ -10,7 +10,7 @@ var calculate = () => {
     fetch(`https://api.exchangerate-api.com/v4/latest/${currencyOne.value}`)
     .then(res => res.json())
     .then(data => {
-        inputTwo.value = (data.rates[currencyTwo.value].toFixed(2) * inputOne.value);
+        inputTwo.value = (data.rates[currencyTwo.value].toFixed(3) * inputOne.value);
         rate.textContent = `1 ${currencyOne.value} = ${data.rates[currencyTwo.value]} ${currencyTwo.value} `;
     })
 };
@@ -27,7 +27,7 @@ swap.addEventListener('click', () => {
     calculate();
 });
 calculate();
-setTimeout(cakculate(), 500);
+setTimeout(calculate(), 500);
 setTimeout(calculate ,500);
 
 
