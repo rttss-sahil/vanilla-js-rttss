8014644577922ea7f73bd8c0d2ce6ade0f63c27f
rttss <sahilrathee55@gmail.com>
2020-07-10 19:53:32 +0530

    jhkklj

--- project3/script.js
+++ project3/script.js
@@ -1,31 +1,58 @@
 const play = document.querySelector(".fa-play");
-const stop = document.querySelector(".fa-stop");
-const fullScreen = document.querySelector(".fa-expand");
+const pause = document.querySelector('.fa-pause');
+const stop = document.querySelector("#stop");
+const forward = document.querySelector("#forward");
+const backward = document.querySelector("#backward");
+const expand = document.querySelector("#expand");
 const video = document.querySelector(".video");
+const progress = document.querySelector("#progress");
+const videoCurrent = document.querySelector(".video-current");
+const videoDuration = document.querySelector(".video-duration");
+const controlList = document.getElementById("controls").classList;
+// SEt Timeout
 
-
-
-// Play/Pause Function + Event Listener
-play.addEventListener("click", playPause = (e) => {
-  if (e.target.classList.contains("fa-play")) {
-    e.target.classList.remove("fa-play");
-    e.target.classList.add("fa-pause");
-    video.play();
+// Visible Controls
+let visible = () => {
+  if (!controlList.contains("visible")) {
+    controlList.add("visible");
   } else {
-    e.target.classList.remove("fa-pause");
-    e.target.classList.add("fa-play");
-    video.pause();
+    controlList.remove("visible");
   }
+  setTimeout(() => {
+    if (controlList.contains("visible")) {
+      controlList.remove("visible"); 
+    }
+  }, 1500);
+};
+// Toggle Video
+let toggleVideo = (e) => {
+  if (video.paused || video.ended) {
+    video.play();
+    play.className = "fa fa-pause";
+  } else {
+    video.pause();
+    play.className = "fa fa-play";
+  }
+};
+// Stop Video
+let stopVideo = (e) => {
+  video.load();
+  play.className = "fa fa-play";
+};
+let updateVideo = () => {
+  progress.value = video.currentTime;
+  
+}
+play.addEventListener("click", toggleVideo);
+// pause.addEventListener('click', toggleVideo);
+stop.addEventListener("click", stopVideo);
+video.addEventListener('dblclick', toggleVideo);
+video.addEventListener("click", visible);
+backward.addEventListener('click', () => {
+  video.load();
+  video.play();
 });
-
-// Stop Function + Event Listener
-stop.addEventListener('click', (e) => {
-    video.load();
-    playPause();
-
-})
-
-progress.addEventListener('click', function(e) {
-    var pos = (e.pageX  - (this.offsetLeft + this.offsetParent.offsetLeft)) / this.offsetWidth;
-    video.currentTime = pos * video.duration;
- });
+expand.addEventListener('click', (e) => {
+  video.requestFullscreen();
+});
+progress.addEventListener('change', updateVideo);
---
+++ project5/script.js
@@ -0,0 +1,30 @@
+currencyOne = document.querySelector('.currency-one');
+currencyTwo = document.querySelector('.currency-two');
+inputOne = document.querySelector('.value-one');
+inputTwo = document.querySelector('.value-two');
+swap = document.querySelector('.swap');
+rate = document.querySelector('.rate');
+
+// Calculate and update 
+var calculate = () => {
+    fetch(`https://api.exchangerate-api.com/v4/latest/${currencyOne.value}`)
+    .then(res => res.json())
+    .then(data => {
+        inputTwo.value = data.rates[currencyTwo.value].toFixed(2) ;
+        rate.textContent = `1 ${currencyOne.value} = ${data.rates[currencyTwo.value]} ${currencyTwo.value} `;
+    })
+};
+
+// Event Listeners
+currencyOne.addEventListener('click', calculate);
+currencyTwo.addEventListener('click', calculate);
+inputOne.addEventListener('change', calculate);
+inputTwo.addEventListener('change', calculate);
+swap.addEventListener('click', () => {
+    let temp = currencyOne.value;
+    currencyOne.value = currencyTwo.value;
+    currencyTwo.value = temp;
+    calculate();
+});
+
+calculate();
