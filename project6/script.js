514882728bd1840f653ee0c8b1c8b6ab360158c2
rttss <sahilrathee55@gmail.com>
2020-07-12 13:00:03 +0530

    ghj

--- project6/script.js
+++ project6/script.js
@@ -1,51 +1,61 @@
-// DOM Manipulation
-const addUser = document.querySelector("#add-user");
-const double = document.querySelector("#double");
-const showM = document.querySelector("#show-millionaires");
-const sort = document.querySelector("#sort");
-const calculate = document.querySelector("#calculate-wealth");
+const addBtn = document.querySelector('#add-user');
+const doubleBtn = document.querySelector('#double');
+const millionBtn = document.querySelector('#show-millionaires');
+const sortBtn = document.querySelector('#sort');
+const totalBtn = document.querySelector('#calculate-wealth');
+const main = document.querySelector('.main');
+let users = [];
+// Generate user Function
+generate();
+generate();
+generate();
+async function generate () {
+    const res = await fetch('https://www.randomuser.me/api');
+    const data = await res.json();
+    user = {
+        name: data.results[0].name.first +" "+ data.results[0].name.last,
+        money: Math.floor(Math.random() * 1000000 )
+    }
+    addToUsers(user);
+}
+var addToUsers = (obj) => {
+    users.push(obj);
+    updateDOM(users);
+}
+var updateDOM = (data = users) => {
+    main.innerHTML = `<h3 class="heading"><strong>Person</strong> Wealth</h3>
+    <ol class="list"></ol>`;
+    data.forEach(item => {
+        const li = document.createElement('li');
+        li.innerHTML = `<h3><strong>${item.name}</strong>$${item.money.toLocaleString('en')}</h3>`;
+        document.querySelector('.main ol').appendChild(li);
+    })
+}
+// Add User Button
+addBtn.addEventListener('click', generate);
+doubleBtn.addEventListener('click', () => {
+    users = users.map((user) => {
+        return {...user, money: user.money * 2}
 
-let list = [];
-// CallBack
-fetchData();
-fetchData();
-fetchData();
+    })
+    updateDOM();
+});
+// Show only Millionaire
+millionBtn.addEventListener('click', () => {
+    users = users.filter(item => item.money > 1000000);
+    updateDOM();
+});
+// Sort 
+sortBtn.addEventListener('click', () => {
+    users = users.sort((a, b) => b.money - a.money );
 
-// Fetch And Update
-async function fetchData() {
-  const res = await fetch("https://randomuser.me/api");
-  const data = await res.json();
-  const user = data.results[0];
-  const name = user.name.first + " " + user.name.last;
-  const money = Math.floor(Math.random() * 1000000);
-  // Add commas to digits
-  updateDOM(name, money.toLocaleString());
-
-}
-// Update DOM
-let updateDOM = (obj1, obj2) => {
-    list.push(obj1, obj2);
-    const li = document.createElement('li');
-    li.innerHTML = `<h3><strong>${list[0]}</strong> $${list[1]}</h3>`;
-    document.querySelector('.list').appendChild(li);
-    list = [];
-}
-// Double Money
-let doubleMoney = () => {
-    return true;
-}
-let showMln = () => {
-    return true;
-}
-let sortData = () => {
-    return true;
-}
-let calculateW = () => {
-    return true;
-}
-// Event LISTENER 
-addUser.addEventListener('click', fetchData);
-double.addEventListener('click', doubleMoney);
-showM.addEventListener('click', showMln);
-sort.addEventListener('click', sortData);
-calculate.addEventListener('click', calculateW);
+    updateDOM();
+});
+// Total Wealth
+totalBtn.addEventListener('click', () => {
+    const wealth = users.reduce((acc, num) => (acc += num.money), 0);
+    const wealthElement = document.createElement('div');
+    wealthElement.classList.add('heading');
+    wealthElement.innerHTML = `<h3><strong>Total Wealth</strong>$${wealth.toLocaleString('en')}</h3>`;
+    main.appendChild(wealthElement);
+})
